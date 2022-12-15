77e669aede89ce4550ee2eef3803c1f82609c0d2
luxD3Br1 <93250849+luxD3Br1@users.noreply.github.com>
2022-12-15 13:53:47 +0800

    J12Kids
    Added new Tab(J12 Kids)
    Still not over bruh

--- Home.js
+++
@@ -1,6 +0,0 @@
-const buttonOpen = document.getElementsByClassName('button')[0]
-const navbarList = document.getElementsByClassName('navbar')[0]
-
-buttonOpen.addEventListener('click', () => {
-  navbarList.classList.toggle('active')
-})
--- sundayService.js
+++ sundayService.js
@@ -3,8 +3,14 @@ const observer = new IntersectionObserver((entries) => {
          if (entry.isIntersecting) {
             entry.target.classList.add('show')
          }
+
+         if (entry.isIntersecting){
+            entry.target.classList.add('show1')
+         }
     });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
+const hidden1Elements = document.querySelectorAll('.hidden1');
 hiddenElements.forEach((el) => observer.observe(el));
+hidden1Elements.forEach((el) => observer.observe(el));
