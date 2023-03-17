3107173f75eeeccd709c58ad86443eb83556bd00
Sêmpiternal <118236448+Sempiternal2k17@users.noreply.github.com>
2023-03-17 15:07:05 +0800

    SundaySchool
    Revamp SundaySchool

--- sundayService.js
+++ sundayService.js
@@ -1,16 +1,17 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-         if (entry.isIntersecting) {
-            entry.target.classList.add('show')
-         }
-
-         if (entry.isIntersecting){
-            entry.target.classList.add('show1')
-         }
-    });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-const hidden1Elements = document.querySelectorAll('.hidden1');
-hiddenElements.forEach((el) => observer.observe(el));
-hidden1Elements.forEach((el) => observer.observe(el));
+const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      navbar.classList.add("show");
+      menuBtn.classList.add("hide");
+      body.classList.add("disabled");
+    }
+    cancelBtn.onclick = ()=>{
+      body.classList.remove("disabled");
+      navbar.classList.remove("show");
+      menuBtn.classList.remove("hide");
+    }
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
