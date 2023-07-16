975a2632e666a09d837668d85d94d959e6d7de78
Sêmpiternal <118236448+Sempiternal2k17@users.noreply.github.com>
2023-07-16 22:55:57 +0800

    Slideshow
    Added some new stuff

--- Index.js
+++ Index.js
@@ -1,3 +1,6 @@
+
+    // ==================================NavBar JS===================================
+
 const body = document.querySelector("body");
     const navbar = document.querySelector(".navbar");
     const menuBtn = document.querySelector(".menu-btn");
@@ -16,32 +19,63 @@ const body = document.querySelector("body");
       this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
     }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+    // ==================================SlideShow JS===================================
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
+    var timeOut = 6000;
+    var slideIndex = 0;
+    var autoOn = true;
+    
+    autoSlides();
+    
+    function autoSlides() {
+        timeOut = timeOut - 20;
+    
+        if (autoOn == true && timeOut < 0) {
+            showSlides();
+        }
+        setTimeout(autoSlides, 20);
+    }
+    
+    function prevSlide() {
+    
+        timeOut = 6000;
+    
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+    
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slideIndex--;
+    
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }
+        if (slideIndex == 0) {
+            slideIndex = 4
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+    
+    function showSlides() {
+    
+        timeOut = 6000;
+    
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+    
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slideIndex++;
+    
+        if (slideIndex > slides.length) {
+            slideIndex = 1
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+    
