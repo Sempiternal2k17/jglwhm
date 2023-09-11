b44710e24576af7e1b3e5c7a2e70721f763a6fb0
Sêmpiternal <118236448+Sempiternal2k17@users.noreply.github.com>
2023-09-11 16:10:00 +0800

    New NavBar
    New Navbar

--- public/Navbar.js
+++ public/Navbar.js
@@ -1,18 +1,34 @@
 
-    const body = document.querySelector("body");
-    const navbar = document.querySelector(".navbar");
-    const menuBtn = document.querySelector(".menu-btn");
-    const cancelBtn = document.querySelector(".cancel-btn");
-    menuBtn.onclick = ()=>{
-      navbar.classList.add("show");
-      menuBtn.classList.add("hide");
-      body.classList.add("disabled");
-    }
-    cancelBtn.onclick = ()=>{
-      body.classList.remove("disabled");
-      navbar.classList.remove("show");
-      menuBtn.classList.remove("hide");
-    }
-    window.onscroll = ()=>{
-      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-    }
+
+//jquery for toggle dropdown menus
+$(document).ready(function(){
+  //toggle sub-menus
+  $(".sub-btn").click(function(){
+    $(this).next(".sub-menu").slideToggle();
+  });
+
+  //toggle more-menus
+  $(".more-btn").click(function(){
+    $(this).next(".more-menu").slideToggle();
+  });
+});
+
+//javascript for the responsive navigation menu
+var menu = document.querySelector(".menu");
+var menuBtn = document.querySelector(".menu-btn");
+var closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
+//javascript for the navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
