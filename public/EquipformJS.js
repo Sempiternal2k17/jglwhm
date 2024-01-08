5eb695a5f1da6f4a9a9e60de0f5e6bb09a683aee
Sêmpiternal <118236448+Sempiternal2k17@users.noreply.github.com>
2024-01-08 10:04:08 +0800

    Final Freaking Update
    Dawd i hate this shit

--- public/EquipformJS.js
+++ public/EquipformJS.js
@@ -1,92 +1,95 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
-import { collection, addDoc, serverTimestamp} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
 import { FooterFunction } from "./Footer.js";
 //   import { query, orderBy, limit, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
 import { firebaseConfig } from "./firebaseConfig.js";
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const equipProgram = urlParams.get('equipProgram');
+const equipProgram = urlParams.get("equipProgram");
 const programColors = {
-    mentoring: '#191825',
-    lifeclass: '#3EB489',
-    solclass: '#1868AE',
+  mentoring: "#191825",
+  lifeclass: "#3EB489",
+  solclass: "#1868AE",
 };
 
 const programTitle = {
-    mentoring: "Mentoring Registration Form",
-    lifeclass: "LifeClass Registration Form",
-    solclass: "Sol Class Registration Form",
-}
+  mentoring: "Mentoring Registration Form",
+  lifeclass: "LifeClass Registration Form",
+  solclass: "Sol Class Registration Form",
+};
 
-let setTitle = programTitle[equipProgram]
-let colorScheme = programColors[equipProgram]
+let setTitle = programTitle[equipProgram];
+let colorScheme = programColors[equipProgram];
 console.log(equipProgram, " ", colorScheme);
-
 
 // ADD FOOTER SECTION
 var footerTemplate = FooterFunction();
-var footerContainer = document.querySelector('.bottomLinks1');
+var footerContainer = document.querySelector(".bottomLinks1");
 footerContainer.innerHTML = footerTemplate;
 
-
-
 // color selection
-function setColorScheme(){
-var navbarDiv = document.querySelector('.nav');
-navbarDiv.style.backgroundColor = `${colorScheme}`;
-var mobileLinksDiv = document.querySelector('.mobileLinks');
-mobileLinksDiv.style.backgroundColor = `${colorScheme}`;
-var bottomLinksDiv = document.querySelector('.bottomLinks');
-bottomLinksDiv.style.backgroundColor = `${colorScheme}`;
-var equipBtn = document.querySelector('#equipFormBtn');
-equipBtn.style.backgroundColor = `${colorScheme}`;
-var equipFormTitle = document.querySelector('#equipFormTitle');
-let parseToCAPS = `${equipProgram} Form`;
-equipFormTitle.innerText = parseToCAPS.toUpperCase();
-var titleProgram = document.querySelector('#titleofProgram');
-titleProgram.innerHTML = `${setTitle}`;
-var submenu = document.querySelector(`.sub-menu`)
-submenu.style.backgroundColor = `${colorScheme}`;
+function setColorScheme() {
+  var navbarDiv = document.querySelector(".nav");
+  navbarDiv.style.backgroundColor = `${colorScheme}`;
+  var mobileLinksDiv = document.querySelector(".mobileLinks");
+  mobileLinksDiv.style.backgroundColor = `${colorScheme}`;
+  var bottomLinksDiv = document.querySelector(".bottomLinks");
+  bottomLinksDiv.style.backgroundColor = `${colorScheme}`;
+  var equipBtn = document.querySelector("#equipFormBtn");
+  equipBtn.style.backgroundColor = `${colorScheme}`;
+  var equipFormTitle = document.querySelector("#equipFormTitle");
+  let parseToCAPS = `${equipProgram} Form`;
+  equipFormTitle.innerText = parseToCAPS.toUpperCase();
+  var titleProgram = document.querySelector("#titleofProgram");
+  titleProgram.innerHTML = `${setTitle}`;
+  var submenu = document.querySelector(`.sub-menu`);
+  submenu.style.backgroundColor = `${colorScheme}`;
 }
 
 setColorScheme();
 //======================================================
 
-let equipForm = document.querySelector('#equipForm');
+let equipForm = document.querySelector("#equipForm");
 
-equipForm.addEventListener('submit', async(event) =>{
-    event.preventDefault();
-    var equipBtn = document.querySelector('#equipFormBtn');
-    equipBtn.style.backgroundColor = '#5a5a5a';
-    equipBtn.disabled = true;
-    try{
-        const fullName = document.getElementById("fullname").value;
-        const email = document.getElementById("email").value;
-        const phoneNumber = document.getElementById("phoneNum").value;
-        const userAge = document.getElementById("userAge").value;
-        const userDataRef = collection(db, `equipping/${equipProgram}/data`);
-        const prayerFormat = {
-            fullname: fullName,
-            email: email,
-            contact: phoneNumber,
-            age: userAge,
-            timestamp: serverTimestamp(),
-        }
-        await addDoc(userDataRef, prayerFormat).then(() => {
-            equipBtn.style.backgroundColor = `${colorScheme}`;
-            equipBtn.disabled = false;
-            if (confirm("Your Information has been submitted!\nReturn to homepage?")) {
-                window.location.href = "./index.html";
-              }
-              else{
-                window.location.reload();
-              }
-            // window.location.href = "./index.html";
-        });
-    }
-    catch(e){throw e};
+equipForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  var equipBtn = document.querySelector("#equipFormBtn");
+  equipBtn.style.backgroundColor = "#5a5a5a";
+
+  try {
+    const fullName = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const phoneNumber = document.getElementById("phoneNum").value;
+    const userAge = document.getElementById("userAge").value;
+    const userDataRef = collection(db, `equipping/${equipProgram}/data`);
+    const prayerFormat = {
+      fullname: fullName,
+      email: email,
+      contact: phoneNumber,
+      age: userAge,
+      timestamp: serverTimestamp(),
+    };
+    await addDoc(userDataRef, prayerFormat).then(() => {
+      equipBtn.style.backgroundColor = `${colorScheme}`;
+
+      if (
+        confirm("Your Information has been submitted!\nReturn to homepage?")
+      ) {
+        window.location.href = "./index.html";
+      } else {
+        window.location.reload();
+      }
+      // window.location.href = "./index.html";
+    });
+  } catch (e) {
+    throw e;
+  }
 });
